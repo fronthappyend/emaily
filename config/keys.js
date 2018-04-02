2b2fa632c4c0b4530082bab5fbbf3d182735d514
Dmytro Beketov <dmytro.beketov@chisw.com>
2018-04-02 14:11:02 +0300

    finishing with oauth

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     googleClientID: "541689485897-i4bgl9vc80o87oa7e23unqqcojqos202.apps.googleusercontent.com",
-    googleClientSecret: "andL-8SexpJxXxH84ciX7-I8"
+    googleClientSecret: "andL-8SexpJxXxH84ciX7-I8",
+    mongoURI: 'mongodb://username:password@ds231549.mlab.com:31549/emaily-dev'
 };
