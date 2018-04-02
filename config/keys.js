03023e577969d42bd3551b57083eed95bb6aca52
Dmytro Beketov <dmytro.beketov@chisw.com>
2018-04-02 15:33:24 +0300

    done with google oauth

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 module.exports = {
     googleClientID: "541689485897-i4bgl9vc80o87oa7e23unqqcojqos202.apps.googleusercontent.com",
     googleClientSecret: "andL-8SexpJxXxH84ciX7-I8",
-    mongoURI: 'mongodb://username:password@ds231549.mlab.com:31549/emaily-dev'
+    mongoURI: 'mongodb://username:password@ds231549.mlab.com:31549/emaily-dev',
+    cookieKey: '123123123'
 };
